26eb0408c6680d1ba5f762d7de94ccb8bd5f6ca4
Andrew Fink <andrewfink@Andrews-MBP.attlocal.net>
2022-06-27 16:53:56 -0500

    adding auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amp2e8d22cc": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
